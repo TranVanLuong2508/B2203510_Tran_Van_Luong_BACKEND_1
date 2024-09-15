523dfdc9153bb5fbea0788c2e1c8b7d7f4f9f626
Tran Van Luong <tranvanluong032020@gmail.com>
2024-09-15 15:30:16 +0700

    Cai dat express chay thong bao xin chao

---
+++ app/config/index.js
@@ -0,0 +1,10 @@
+const config = {
+    app: {
+      port: process.env.PORT || 3000,
+    },
+    db: {
+      uri: process.env.MONGODB_URI || "mongodb+srv://tranthaitoan999:29022004@ct449.m6vo3ck.mongodb.net/contactbook?retryWrites=true&w=majority&appName=CT449"
+      }
+  };
+  
+  module.exports = config;
