6be97431c0e9ce7890a403d23c41e000ce5d122e
Tran Van Luong <tranvanluong032020@gmail.com>
2024-09-19 16:18:40 +0700

    update

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-// const app =require("./app")
-// const config = require("./app/config/index")
+const app =require("./app")
+const config = require("./app/config/index")
 
-// //start server
-// const PORT = config.app.port
-// app.listen(PORT, () =>{
-//     console.log(`Server is running on port ${PORT}`)
-// })
+//start server
+const PORT = config.app.port
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
 
 
-
-const app = require("./app.js")
-const config = require("./app/config/index.js")
-const MongoDB = require("./app/utils/mongodb.util.js")
-
-async function startServer() {
-    try {
-        await MongoDB.connect(config.db.uri)
-        console.log("Connected to the database")
-        const PORT = config.app.port
-        app.listen(PORT, () =>{
-            console.log(`Server is running on port ${PORT}`)
-        })
-    } catch (error) {
-        console.log("Cannot connect to the database", error)
-        process.exit()
-    }
-}
-
-startServer()
